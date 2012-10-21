42082b533f869c1e3dc3fced0ed339e26289e53a
soumya mandi <soumyamandi@gmail.com>
2012-10-22 03:49:26 +0530

    makefile added and insert method complete

--- btree.c
+++ btree.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-typdef struct {
+typedef struct {
 
 	struct node* left_child ;
 	struct node* right_child ;
@@ -7,7 +7,56 @@ typdef struct {
 
 } node ;
 
-int main( int argc,char** argv ) {
+void bfs_recursive( node* root ) {
+	node* current = root ;
+	if( current ) {
+		printf("%d \n",current->value ) ;
+		bfs_recursive( current->left_child ) ;
+		bfs_recursive( current->right_child ) ;
 
+	}
+
+
+
+	
+}
+
+/* inserts a new node */
+node* insert( node* root, int value ) {
+	if( !root ) {
+		printf("hehe\n") ;
+		root = ( node* )malloc( sizeof( int ) ) ;
+		root->value = value ;
+		root->left_child = ( node* )NULL ;
+		root->right_child = ( node* )NULL ;
+		return root ;
+	}
+	else {
+		if( value < root->value ) {
+			root->left_child = insert( root->left_child,value ) ;
+			return root ;
+		}
+		else {
+			root->right_child = insert( root->right_child,value ) ;
+			return root ;
+		}
+
+	}
+
+}
+
+node* initialize_tree( int arr[], int size ) {
+	node* root = NULL ;
+	int i ;
+	for( i = 0 ; i < size ; i++ ) {
+		insert( root,arr[i] ) ;
+	}
+	return root ;
+}
+
+int main( int argc,char** argv ) {
+	int arr[] = { 1,2,3,4,5 } ;
+	node* root = initialize_tree( arr,5 ) ;
+	bfs_recursive( root ) ;
 	return 0 ;
 }
